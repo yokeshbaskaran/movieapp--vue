fbe7b6555705444312b19cb0278f8adbacfd6c1f
Yoki <thekingmaker0012@gmail.com>
2024-05-04 10:33:19 +0530

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <RouterLink to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </RouterLink>
+  </header>
+
+  <main>
+    <router-view />
+  </main>
+
+  <footer>CopyRights &copy; 2024</footer>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+  &::selection {
+    background: transparentize($color: #428883, $amount: 0.5);
+  }
+  text-decoration: none;
 }
 
-nav {
-  padding: 30px;
+body {
+  background-color: #35495e;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.1);
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+h1 {
+  color: #fff;
+  font-size: 28px;
+
+  span {
+    color: #42b883;
   }
 }
 </style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+  apiKey: "358f15cc",
+};
