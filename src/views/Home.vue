3658fcbf6372141146f23909e8b5601b62a2717c
YOKESH B <161866896+yokeshbaskaran@users.noreply.github.com>
2024-05-22 18:10:39 +0530

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,7 +59,7 @@ export default {
 
     const SearchMovies = () => {
       if (search.value != "") {
-        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+        fetch(`https://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
           .then((response) => response.json())
           .then((data) => {
             movies.value = data.Search;
